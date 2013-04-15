6904e582421840da10c64ecf7e9d138d1e735dce
Gian Merlino <gianmerlino@gmail.com>
2013-04-15 12:56:54 -0700

    Emitting: Replace implicits with statics (like EmittingLogger)

--- src/main/scala/com/metamx/common/scala/event/Emitting.scala
+++ src/main/scala/com/metamx/common/scala/event/Emitting.scala
@@ -2,21 +2,20 @@ package com.metamx.common.scala.event
 
 import com.metamx.common.scala.Logging
 import com.metamx.common.scala.untyped.Dict
-import com.metamx.common.scala.Logging._
 import Emitting._
+import com.metamx.common.scala.LateVal.LateVal
+import com.metamx.emitter.EmittingLogger
 
 trait Emitting extends Logging
 {
-  implicit val emitter: ServiceEmitter = implicitly[ServiceEmitter]
+  def emitter: ServiceEmitter = Emitting.emitter
 
   def emitAlert(e: Throwable, severity: Severity, description: String, data: Dict)
-    (implicit log: Logger, emitter: ServiceEmitter)
   {
     emit.emitAlert(e, log, emitter, severity, description, data)
   }
 
   def emitAlert(severity: Severity, description: String, data: Dict)
-    (implicit log: Logger, emitter: ServiceEmitter)
   {
     emit.emitAlert(null, log, emitter, severity, description, data)
   }
@@ -26,4 +25,13 @@ object Emitting
 {
   type ServiceEmitter = com.metamx.emitter.service.ServiceEmitter
   type Severity = com.metamx.emitter.service.AlertEvent.Severity
+
+  private val emitterLateVal = new LateVal[ServiceEmitter]
+
+  def emitter = emitterLateVal.deref
+
+  def emitter_=(_emitter: ServiceEmitter) {
+    emitterLateVal.assign(_emitter)
+    EmittingLogger.registerEmitter(_emitter)
+  }
 }
