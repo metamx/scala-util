98bd486cc7c30ff6f2f7922632f415a390d9d15d
Gian Merlino <gianmerlino@gmail.com>
2014-06-02 17:35:49 -0700

    DiscoResolver: Curator-service-discovery-based Finagle resolver.

---
+++ src/main/scala/com/metamx/common/scala/net/finagle/DiscoResolver.scala
@@ -0,0 +1,63 @@
+package com.metamx.common.scala.net.finagle
+
+import com.metamx.common.lifecycle.Lifecycle
+import com.metamx.common.scala.Logging
+import com.metamx.common.scala.net.curator.Disco
+import com.twitter.finagle.Addr
+import com.twitter.finagle.Resolver
+import com.twitter.util.Closable
+import com.twitter.util.Future
+import com.twitter.util.Time
+import com.twitter.util.Var
+import java.net.{SocketAddress, InetSocketAddress}
+import org.apache.curator.framework.CuratorFramework
+import org.apache.curator.framework.state.ConnectionState
+import org.apache.curator.x.discovery.details.ServiceCacheListener
+import scala.collection.JavaConverters._
+
+class DiscoResolver(disco: Disco) extends Resolver with Logging
+{
+  val scheme = "disco"
+
+  def bind(service: String) = Var.async[Addr](Addr.Pending) {
+    updatable =>
+      val lifecycle = new Lifecycle
+      val serviceCache = disco.cacheFor(service, lifecycle)
+      def doUpdate() {
+        val newInstances = serviceCache.getInstances.asScala.toSet
+        log.info("Updating instances for service[%s] to %s", service, newInstances)
+        val newSocketAddresses: Set[SocketAddress] = newInstances map
+          (instance => new InetSocketAddress(instance.getAddress, instance.getPort))
+        updatable.update(Addr.Bound(newSocketAddresses))
+      }
+      serviceCache.addListener(
+        new ServiceCacheListener
+        {
+          def cacheChanged() {
+            doUpdate()
+          }
+
+          def stateChanged(curator: CuratorFramework, state: ConnectionState) {
+            doUpdate()
+          }
+        }
+      )
+      lifecycle.start()
+      try {
+        doUpdate()
+        new Closable
+        {
+          def close(deadline: Time) = Future {
+            log.info("No longer monitoring service[%s]", service)
+            lifecycle.stop()
+          }
+        }
+      }
+      catch {
+        case e: Exception =>
+          log.warn(e, "Failed to bind to service[%s]", service)
+          lifecycle.stop()
+          throw e
+      }
+  }
+}
