04ed6c0e0c0854d847b8da7bf17900e9228797d2
Alex Demidko <dralexthk@gmail.com>
2014-06-12 15:26:34 -0700

    Runner threads

---
+++ src/main/scala/com/metamx/common/threads.scala
@@ -0,0 +1,49 @@
+package com.metamx.common
+
+import com.metamx.common.scala.Logging
+import com.metamx.common.scala.concurrent._
+import org.scala_tools.time.TypeImports._
+
+object threads extends Logging
+{
+  def runnerThread(name: String, f: => Any): Thread = {
+    runnerThread(name, None, f)
+  }
+
+  def runnerThread(name: String, quietPeriod: Period, f: => Any): Thread = {
+    runnerThread(name, Some(quietPeriod), f)
+  }
+
+  private def runnerThread(name: String, quietPeriod: Option[Period], f: => Any): Thread = {
+    val runnable = loggingRunnable {
+      val quietMillis = quietPeriod.map(_.toStandardDuration.getMillis)
+
+      while (!Thread.currentThread().isInterrupted) {
+        val startMillis = System.currentTimeMillis()
+        try {
+          f
+        } catch {
+          case e: Exception => log.error(e, "Exception while running %s".format(name))
+        }
+
+        quietMillis match {
+          case Some(m) =>
+            val waitMillis = startMillis + m - System.currentTimeMillis()
+            if (waitMillis > 0) {
+              Thread.sleep(waitMillis)
+            }
+
+          case None => // Don't need to sleep
+        }
+
+      }
+    }
+
+    val thread = new Thread(runnable)
+    thread.setName(name)
+    thread.setDaemon(true)
+    thread.start()
+
+    thread
+  }
+}
