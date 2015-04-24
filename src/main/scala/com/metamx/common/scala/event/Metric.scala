bb3d1a3f49175b3b85ed3964db456f8b5324205b
Maksim Logvinenko <mlogvinenko@gmail.com>
2015-04-24 15:33:06 +0300

    Make Metric schemaless. Add MetricTest

--- src/main/scala/com/metamx/common/scala/event/Metric.scala
+++ src/main/scala/com/metamx/common/scala/event/Metric.scala
@@ -6,19 +6,10 @@ import com.metamx.common.scala.untyped._
 
 // A partially constructed ServiceEventBuilder[ServiceMetricEvent]. Immutable, unlike ServiceMetricEvent.Builder.
 case class Metric(
-  metric:  String           = null,
-  value:   Number           = null,
-  user1:   Iterable[String] = null,
-  user2:   Iterable[String] = null,
-  user3:   Iterable[String] = null,
-  user4:   Iterable[String] = null,
-  user5:   Iterable[String] = null,
-  user6:   Iterable[String] = null,
-  user7:   Iterable[String] = null,
-  user8:   Iterable[String] = null,
-  user9:   Iterable[String] = null,
-  user10:  Iterable[String] = null,
-  created: DateTime         = null
+  metric:   String                        = null,
+  value:    Number                        = null,
+  userDims: Map[String, Iterable[String]] = null,
+  created:  DateTime                      = null
   ) extends ServiceEventBuilder[ServiceMetricEvent] {
 
   // Join two partially constructed metrics; throw IllegalArgumentException if any field is defined on both
@@ -31,37 +22,49 @@ case class Metric(
         "%s already defined as %s, refusing to shadow with %s" format (desc, x, y)
       )
     }
+
+    def intersectMap(x:Map[String, Iterable[String]], y:Map[String, Iterable[String]]): Map[String, Iterable[String]] = {
+      (Option(x), Option(y)) match {
+        case (None,    None)    => null
+        case (Some(x), None)    => x
+        case (None,    Some(y)) => y
+        case (Some(x), Some(y)) => {
+          x.keySet & y.keySet toSeq match {
+            case Seq() => x ++ y
+            case r => throw new IllegalArgumentException(
+              "userDims has common keys: %s" format r mkString ","
+            )
+          }
+        }
+      }
+    }
+
     Metric(
-      metric  = f(this.metric,  that.metric,  "metric"),
-      value   = f(this.value,   that.value,   "value"),
-      user1   = f(this.user1,   that.user1,   "user1"),
-      user2   = f(this.user2,   that.user2,   "user2"),
-      user3   = f(this.user3,   that.user3,   "user3"),
-      user4   = f(this.user4,   that.user4,   "user4"),
-      user5   = f(this.user5,   that.user5,   "user5"),
-      user6   = f(this.user6,   that.user6,   "user6"),
-      user7   = f(this.user7,   that.user7,   "user7"),
-      user8   = f(this.user8,   that.user8,   "user8"),
-      user9   = f(this.user9,   that.user9,   "user9"),
-      user10  = f(this.user10,  that.user10,  "user10"),
-      created = f(this.created, that.created, "created")
+      metric    = f(this.metric,  that.metric,  "metric"),
+      value     = f(this.value,   that.value,   "value"),
+      userDims  = intersectMap(this.userDims, that.userDims),
+      created   = f(this.created, that.created, "created")
     )
+  }
+
+  def +(name: String, value: String): Metric = {
+    this + Metric(userDims = Map(name -> Seq(value)))
+  }
+
+  def +(name: String, value: Iterable[String]): Metric = {
+    this + Metric(userDims = Map(name -> value))
   }
 
   // Build into a ServiceMetricEvent, throwing NullPointerException if any required field is null
   override def build(service: String, host: String) = {
     val builder = ServiceMetricEvent.builder()
 
-    if (user1  != null) { builder.setUser1  (user1.toArray)  }
-    if (user2  != null) { builder.setUser2  (user2.toArray)  }
-    if (user3  != null) { builder.setUser3  (user3.toArray)  }
-    if (user4  != null) { builder.setUser4  (user4.toArray)  }
-    if (user5  != null) { builder.setUser5  (user5.toArray)  }
-    if (user6  != null) { builder.setUser6  (user6.toArray)  }
-    if (user7  != null) { builder.setUser7  (user7.toArray)  }
-    if (user8  != null) { builder.setUser8  (user8.toArray)  }
-    if (user9  != null) { builder.setUser9  (user9.toArray)  }
-    if (user10 != null) { builder.setUser10 (user10.toArray) }
+    val userDimsOpt = Option(userDims)
+    if (userDimsOpt.isDefined) {
+      userDimsOpt.get map  {
+        case(k, v) => builder.setDimension(k, v.toArray)
+      }
+    }
 
     builder.build(created, noNull(metric), noNull(value))
       .build(noNull(service), noNull(host))
---
+++ src/test/scala/com/metamx/common/scala/event/MetricTest.scala
@@ -0,0 +1,68 @@
+package com.metamx.common.scala.event
+
+import com.simple.simplespec.Matchers
+import org.junit.Test
+import org.scala_tools.time.Imports._
+
+
+class MetricTest extends Matchers
+{
+  @Test
+  def testUnionMetrics() {
+    val m1 = Metric(metric = "metric")
+    val m2 = Metric(value = 1)
+
+    val res = m1 + m2
+
+    res.metric must be("metric")
+
+    res.value must be(1)
+  }
+
+  @Test
+  def testUnionWithException() {
+    val m1 = Metric(metric = "metric1", value = 1, created = new DateTime())
+    val m2 = Metric(metric = "metric2", value = 2, created = new DateTime())
+
+    evaluating {
+      m1 + m2
+    } must throwAn[IllegalArgumentException]
+  }
+
+  @Test
+  def testUnionDimensions() {
+    (Metric() + ("dim", "val")).userDims.get("dim").isDefined must be(true)
+    (Metric() + ("dim", Seq("val"))).userDims.get("dim").isDefined must be(true)
+
+    evaluating {
+      Metric(userDims = Map("dim" -> Seq("value1"))) + Metric(userDims = Map("dim" -> Seq("value2")))
+    } must throwAn[IllegalArgumentException]
+
+    val res = Metric(userDims = Map("dim1" -> Seq("value1"))) +
+      Metric(userDims = Map("dim2" -> Seq("value2.1", "value2.2")))
+
+    res.userDims.get("dim1").isDefined must be(true)
+    res.userDims.get("dim1").get.head must be("value1")
+
+    res.userDims.get("dim2").isDefined must be(true)
+    res.userDims.get("dim2").get.head must be("value2.1")
+    res.userDims.get("dim2").get.size must be(2)
+  }
+
+  @Test
+  def testBuildServiceMetric() {
+    val event = Metric(
+      metric = "metric",
+      value = 1,
+      userDims = Map("dim1" -> Seq("value1"), "dim2" -> Seq("value2.1", "value2.2"))
+    ).build("test", "localhost")
+
+    event.getService  must be ("test")
+    event.getHost     must be ("localhost")
+    event.getMetric   must be ("metric")
+    event.getValue    must be (1)
+
+    event.getUserDims.containsKey("dim1") must be(true)
+    event.getUserDims.containsKey("dim2") must be(true)
+  }
+}
